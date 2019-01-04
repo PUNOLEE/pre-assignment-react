6ee775c003c8a58e365b6b228f0b01b5d64667ef
PUNOLEE <punolee@outlook.com>
2019-01-03 20:33:01 -0600

    Click makes X #2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -31,9 +31,10 @@ class Board extends React.Component {
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = 'X';
+    
     this.setState({squares: squares});
   }
-  
+
   renderSquare(i) {
     return  (<Square 
       value={this.state.squares[i]} 
