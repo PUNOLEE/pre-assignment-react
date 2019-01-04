c28ce5047c8f5794bb59e0c7dc28956d8210a520
PUNOLEE <punolee@outlook.com>
2019-01-03 20:32:25 -0600

    click alerts & click makes X #1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
+      <button className="square" 
+      onClick={() => this.props.onClick()}
+      >
         {this.props.value}
       </button>
     );
@@ -12,8 +21,23 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+  
   renderSquare(i) {
-    return <Square value={i} />;
+    return  (<Square 
+      value={this.state.squares[i]} 
+      onClick={() => this.handleClick(i)} />);
   }
 
   render() {
